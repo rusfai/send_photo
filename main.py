3abe81be54325fb12958e582a5d8bbd8a337d25c
rusfai <70482477+rusfai@users.noreply.github.com>
2024-09-23 23:26:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ app = Flask(__name__)
 
 @app.route('/tiktok', methods=['POST', 'GET'])
 def webhook():
+    print("12435")
     asyncio.get_event_loop().run_until_complete(gg(5779182088, str('Пришло')))
     try:
         data = request.args.get("tiktok")
