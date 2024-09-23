ba5ec61b43df6fea3f225cadb131d76356202538
rusfai <70482477+rusfai@users.noreply.github.com>
2024-09-23 13:38:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from flask import Flask, jsonify, request, redirect
+import asyncio
+from aiogram import Bot
+from aiogram import types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+token = '5766763452:AAG14hwIr0o3pxAN1JRYlnk7hhIeH1CobXM'
+
+
+bot = Bot(token)
+
+
+async def gg(user_id, text):
+
+    
+
+    await bot.send_message(user_id, f'{text}!')
+
+    return 'sucsess'
 
 
 app = Flask(__name__)
@@ -7,9 +27,12 @@ app = Flask(__name__)
 def webhook():
     try:
         data = request.get_json(force=False, silent=False, cache=True)
-        print(data)
+
+        asyncio.get_event_loop().run_until_complete(gg(5779182088, str(data)))
+
         return 'sucsess'
     except:
+        asyncio.get_event_loop().run_until_complete(gg(5779182088, str('Ошибка')))
         return 'warning' 
 
 
