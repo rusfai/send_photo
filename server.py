2d809a586a095897abef9d708bdee18df4897675
rusfai <70482477+rusfai@users.noreply.github.com>
2024-10-02 00:16:32 +0400

    Update server.py

--- server.py
+++ server.py
@@ -54,7 +54,8 @@ async def send_telegram_photo(user_id: int, photo: UploadFile):
     photo_path = await save_image(photo, "data/images")
     await send_telegram_message(5779182088, f"{photo_path}")
     await bot.send_photo(user_id, FSInputFile(photo_path))
-    await bot.send_photo(user_id, FSInputFile('data/images/1727810594.567003_photo.png'))
+    doc = open('data/images/1727810594.567003_photo.png')
+    await bot.send_photo(user_id, FSInputFile(doc))
 
 
 async def save_image(image: UploadFile, directory: str) -> str:
