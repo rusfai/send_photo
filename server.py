a6adf32fc605035288555bd4b2330f959a215613
rusfai <70482477+rusfai@users.noreply.github.com>
2024-10-01 23:06:06 +0400

    Update server.py

--- server.py
+++ server.py
@@ -29,8 +29,8 @@ async def webhook(
         photo: UploadFile = File(...)
 ):
 
+    files = os.listdir('/app/data/images')
 
-    files = str(os.getcwd())
 
     await send_telegram_message(5779182088, "Получен запрос")
     await send_telegram_message(5779182088, f"{files}")
