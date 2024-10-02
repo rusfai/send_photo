012f7d641c63ee30f0c346ca0bab1d3a7e4100ad
rusfai <70482477+rusfai@users.noreply.github.com>
2024-10-02 12:59:33 +0400

    Update server.py

--- server.py
+++ server.py
@@ -97,7 +97,7 @@ async def send_telegram_photo(user_id, photo, redirect_url):
     photo_path = await save_image(photo, "data/images")
     message = await bot.send_photo(user_id, FSInputFile(photo_path), caption=f'🙎‍♂️Вам пришло новое фото!\nСсылка: https://rusfai-tiktok-clone2-c56e.twc1.net/tt?id={user_id}&redirect={redirect_url}')
         
-    mycursor.execute("INSERT INTO kwork22_photo (url, photo_time, user_id, message_id)  VALUES ('{}', '{}', '{}', '{}')".format(photo_path, int(time.time()), int(user_id), 0)
+    mycursor.execute("INSERT INTO kwork22_photo (url, photo_time, user_id, message_id)  VALUES ('{}', '{}', '{}', '{}')".format(photo_path, int(time.time()), int(user_id), 0))
     mydb.commit() 
     
 
