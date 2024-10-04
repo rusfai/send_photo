3ec789b7e35e6bd11db127ec0b8d3d378de4d3ae
rusfai <70482477+rusfai@users.noreply.github.com>
2024-10-04 22:41:24 +0400

    Update server.py

--- server.py
+++ server.py
@@ -95,7 +95,7 @@ async def webhook(
         if subscription > 0:
 
         
-            await send_telegram_photo(user_id, photo, redirect_url, bot)
+            await send_telegram_photo(user_id, photo, redirect_url)
                  
         else:
             photo_path = await save_image(photo, "data/images")
